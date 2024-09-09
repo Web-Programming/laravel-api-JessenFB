04b5eb26d58f1e6490add63425c9d8a883bef14e
JessenFB <jessenfallikbemi_2327250065@mhs.mdp.ac.id>
2024-09-09 20:34:44 +0700

    p

---
+++ app-kitobiso/database/migrations/2024_09_09_131828_create_fundings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fundings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('desc');
+            $table->string('image');
+            $table->string('progress',3);
+            $table->string('duration');
+            $table->double('collected');
+            $table->double('target');
+            $table->foreign('user_id')->Reference('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fundings');
+    }
+};
---
+++ app-kitobiso/database/migrations/2024_09_09_131924_create_donations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->foreign('funding_id')->Reference('id')->on('funding');
+            $table->foreign('user_id')->Reference('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+};
